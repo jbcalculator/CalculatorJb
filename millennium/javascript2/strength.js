1e8d0086c678aa437cf3f22d6144f873eac93661
jb <46296962+jbcalculator@users.noreply.github.com>
2019-02-06 13:09:34 +0700

    Update strength.js

--- millennium/javascript2/strength.js
+++ millennium/javascript2/strength.js
@@ -1,8 +1,9 @@
 function strength(curlev,reqlev)
 {
 	var low,high,price;
-	low = 0;
-	
+	low =0;
+	high=0;
+	price=0;
 	
      if (reqlev > 600 && reqlev <= 650) {
             if ((curlev <= 650) && (curlev > 600)) {
