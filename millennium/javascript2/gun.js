867a6cd1771ba6f7385f98225031d08ef31468e1
jb <46296962+jbcalculator@users.noreply.github.com>
2019-02-23 23:16:27 +0700

    Update gun.js

--- millennium/javascript2/gun.js
+++ millennium/javascript2/gun.js
@@ -55,7 +55,7 @@ function gun(curlev,reqlev)
                 low *= 2;
                 price = high + low;
             } else if (curlev <= 650) {
-                price = reqlev - 450;
+                price = reqlev - curlev;
                 price *= 2.5;
             }
         } else if (reqlev >= 350) {
