5f3692de46f18d0f489654a2d34245afc879f835
jb <46296962+jbcalculator@users.noreply.github.com>
2019-02-23 18:10:28 +0700

    Update gun.js

--- millennium/javascript2/gun.js
+++ millennium/javascript2/gun.js
@@ -5,351 +5,90 @@ function gun(curlev,reqlev)
 	high = 0;
 	price = 0;
 		
-    if (reqlev > 800 && reqlev <= 850) {
-            if (curlev <= 850 && curlev > 800) {
-                price = reqlev - curlev;
-                price *= 5.0;
-            }
-            if (curlev <= 800 && curlev > 700) {
-                low = 800 - curlev;
-                low *= 4.5;
-                high = reqlev - 800;
-                high *= 5.0;
-                price = high + low;
-            }
-            if (curlev <= 700 && curlev > 600) {
-                low = 700 - curlev;
-                low *= 4.0;
-                high = reqlev - 800;
-                high *= 5.0;
-                price = high + low + 150;
-            }
-            if ((curlev <= 600) && (curlev > 500)) {
-                low = 600 - curlev;
-                low *= 3.5;
-                high = reqlev - 800;
-                high *= 5.0;
-                price = high + low + 350;
-            }
-            if ((curlev <= 500) && (curlev > 400)) {
-                low = 500 - curlev;
-                low *= 3.0;
-                high = reqlev - 800;
-                high *= 5.0;
-                price = high + low + 600;
-
-            }
-            if ((curlev <= 400) && (curlev > 300)) {
-                low = 400 - curlev;
-                low *= 2.5;
-                high = reqlev - 800;
-                high *= 5.0;
-                price = high + low + 900;
-            }
-            if ((curlev <= 300) && (curlev > 200)) {
-
-                low = 300 - curlev;
-                low *= 2.0;
-                high = reqlev - 800;
-                high *= 5.0;
-                price = high + low + 1250;
-            }
-            if ((curlev <= 200) && (curlev > 100)) {
-
-                low = 200 - curlev;
+     if (reqlev > 650 && reqlev <= 850) {
+            if (curlev <= 250) {
+                high = reqlev - 650;
+                high *= 3;
+                low = 250 - curlev;
+                low *= 1;
+                price = high + low + 850;
+            } else if (curlev <= 350) {
+                high = reqlev - 650;
+                high *= 3;
+                low = 350 - curlev;
                 low *= 1.5;
-                high = reqlev - 800;
-                high *= 5.0;
-                price = high + low + 1650;
-            }
-            if ((curlev <= 100) && (curlev > 0)) {
-                low = 100 - curlev;
-                low *= 1.0;
-                high = reqlev - 800;
-                high *= 5.0;
-                price = high + low + 2100;
-            }
-        } else if (reqlev > 700 && reqlev <= 800) {
-            if (curlev <= 800 && curlev > 700) {
-                price = reqlev - curlev;
-                price *= 4.5;
-            }
-            if (curlev <= 700 && curlev > 600) {
-                low = 700 - curlev;
-                low *= 4.0;
-                high = reqlev - 700;
-                high *= 4.5;
+                price = high + low + 700;
+            } else if (curlev <= 450) {
+                high = reqlev - 650;
+                high *= 3;
+                low = 450 - curlev;
+                low *= 2;
+                price = high + low + 500;
+            } else if (curlev <= 650) {
+                high = reqlev - 650;
+                high *= 3;
+                low = 650 - curlev;
+                low *= 2.5;
                 price = high + low;
+                System.out.println(price);
+            } else if (curlev <= 850) {
+                price = reqlev - curlev;
+                price *= 3;
             }
-            if ((curlev <= 600) && (curlev > 500)) {
-                low = 600 - curlev;
-                low *= 3.5;
-                high = reqlev - 700;
-                high *= 4.5;
+        } else if (reqlev > 450) {
+            if (curlev <= 250) {
+                high = reqlev - 450;
+                high *= 2.5;
+                low = 250 - curlev;
+                low *= 1;
+                price = high + low + 350;
+            } else if (curlev <= 350) {
+                high = reqlev - 450;
+                high *= 2.5;
+                low = 350 - curlev;
+                low *= 1.5;
                 price = high + low + 200;
-
-            }
-            if ((curlev <= 500) && (curlev > 400)) {
-                low = 500 - curlev;
-                low *= 3.0;
-                high = reqlev - 700;
-                high *= 4.5;
-                price = high + low + 350;
-
-            }
-            if ((curlev <= 400) && (curlev > 300)) {
-                low = 400 - curlev;
-                low *= 2.5;
-                high = reqlev - 700;
-                high *= 4.5;
-                price = high + low + 600;
-
-            }
-            if ((curlev <= 300) && (curlev > 200)) {
-
-                low = 300 - curlev;
-                low *= 2.0;
-                high = reqlev - 700;
-                high *= 4.5;
-                price = high + low + 900;
-
-            }
-            if ((curlev <= 200) && (curlev > 100)) {
-
-                low = 200 - curlev;
-                low *= 1.5;
-                high = reqlev - 700;
-                high *= 4.5;
-                price = high + low + 1250;
-
-            }
-            if ((curlev <= 100) && (curlev > 0)) {
-                low = 100 - curlev;
-                low *= 1.0;
-                high = reqlev - 700;
-                high *= 4.5;
-                price = high + low + 1650;
-            }
-        } else if (reqlev > 600 && reqlev <= 700) {
-            if ((curlev <= 650) && (curlev > 600)) {
-                price = reqlev - curlev;
-                price *= 4.0;
-
-            }
-            if ((curlev <= 600) && (curlev > 500)) {
-                low = 600 - curlev;
-                low *= 3.5;
-                high = reqlev - 600;
-                high *= 4.0;
+            } else if (curlev <= 450) {
+                high = reqlev - 450;
+                high *= 2.5;
+                low = 450 - curlev;
+                low *= 2;
                 price = high + low;
-
-            }
-            if ((curlev <= 500) && (curlev > 400)) {
-                low = 500 - curlev;
-                low *= 3.0;
-                high = reqlev - 600;
-                high *= 3.5;
-                price = high + low + 150;
-
-            }
-            if ((curlev <= 400) && (curlev > 300)) {
-                low = 400 - curlev;
-                low *= 2.5;
-                high = reqlev - 600;
-                high *= 4.0;
-                price = high + low + 350;
-
-            }
-            if ((curlev <= 300) && (curlev > 200)) {
-
-                low = 300 - curlev;
-                low *= 2.0;
-                high = reqlev - 600;
-                high *= 4.0;
-                price = high + low + 600;
-
-            }
-            if ((curlev <= 200) && (curlev > 100)) {
-
-                low = 200 - curlev;
-                low *= 1.5;
-                high = reqlev - 600;
-                high *= 4.0;
-                price = high + low + 900;
-
-            }
-            if ((curlev <= 100) && (curlev > 0)) {
-                low = 100 - curlev;
-                low *= 1.0;
-                high = reqlev - 600;
-                high *= 4.0;
-                price = high + low + 1250;
-
-            }
-
-        } else if (reqlev > 500 && reqlev <= 600) {
-            if ((curlev <= 600) && (curlev > 500)) {
-                price = reqlev - curlev;
-                price *= 3.5;
-
-            }
-            if ((curlev <= 500) && (curlev > 400)) {
-                low = 500 - curlev;
-                low *= 3.0;
-                high = reqlev - 500;
-                high *= 3.5;
-                price = high + low;
-
-            }
-            if ((curlev <= 400) && (curlev > 300)) {
-                low = 400 - curlev;
-                low *= 2.5;
-                high = reqlev - 500;
-                high *= 3.5;
-                price = high + low + 150;
-
-            }
-            if ((curlev <= 300) && (curlev > 200)) {
-                low = 300 - curlev;
-                low *= 2.0;
-                high = reqlev - 500;
-                high *= 3.5;
-                price = high + low + 350;
-
-            }
-            if ((curlev <= 200) && (curlev > 100)) {
-
-                low = 200 - curlev;
-                low *= 1.5;
-                high = reqlev - 500;
-                high *= 3.5;
-                price = high + low + 600;
-
-            }
-            if ((curlev <= 100) && (curlev > 0)) {
-                low = 100 - curlev;
-                low *= 1.0;
-                high = reqlev - 500;
-                high *= 3.5;
-                price = high + low + 900;
-
-            }
-        } else if (reqlev > 400 && reqlev <= 500) {
-            if ((curlev <= 500) && (curlev > 400)) {
-                price = reqlev - curlev;
-                price *= 3.0;
-
-            }
-            if ((curlev <= 400) && (curlev > 300)) {
-                low = 400 - curlev;
-                low *= 2.5;
-                high = reqlev - 400;
-                high *= 3.0;
-                price = high + low;
-
-            }
-            if ((curlev <= 300) && (curlev > 200)) {
-                low = 300 - curlev;
-                low *= 2.0;
-                high = reqlev - 400;
-                high *= 3.0;
-                price = high + low + 150;
-
-            }
-            if ((curlev <= 200) && (curlev > 100)) {
-                low = 200 - curlev;
-                low *= 1.5;
-                high = reqlev - 400;
-                high *= 3.0;
-                price = high + low + 350;
-
-            }
-            if ((curlev <= 100) && (curlev > 000)) {
-                low = 100 - curlev;
-                low *= 1.0;
-                high = reqlev - 400;
-                high *= 3.0;
-                price = high + low + 600;
-
-            }
-
-        } else if (reqlev > 300 && reqlev <= 400) {
-
-            if ((curlev <= 400) && (curlev > 300)) {
-                price = reqlev - curlev;
+            } else if (curlev <= 650) {
+                price = reqlev - 450;
                 price *= 2.5;
-
             }
-            if ((curlev <= 300) && (curlev > 200)) {
-                low = 300 - curlev;
-                low *= 2.0;
-                high = reqlev - 300;
-                high *= 2.5;
-                price = high + low;
-
-            }
-            if ((curlev <= 200) && (curlev > 100)) {
-                low = 200 - curlev;
-                low *= 1.5;
-                high = reqlev - 300;
-                high *= 2.5;
+        } else if (reqlev >= 350) {
+            if (curlev <= 250) {
+                high = reqlev - 350;
+                high *= 2;
+                low = 250 - curlev;
+                low *= 1;
                 price = high + low + 150;
-
-            }
-            if ((curlev <= 100) && (curlev > 0)) {
-                low = 100 - curlev;
-                low *= 1.0;
-                high = reqlev - 300;
-                high *= 2.5;
-                price = high + low + 350;
-
-            }
-        } else if (reqlev > 200 && reqlev <= 300) {
-
-            if ((curlev <= 300) && (curlev > 200)) {
+            } else if (curlev <= 350) {
+                high = reqlev - 350;
+                high *= 2;
+                low = 350 - curlev;
+                low *= 1.5;
+                price = high + low;
+            } else if (curlev <= 450) {
                 price = reqlev - curlev;
-                price *= 2.0;
-
+                price *= 2;
             }
-            if ((curlev <= 200) && (curlev > 100)) {
-                low = 200 - curlev;
-                low *= 1.5;
-                high = reqlev - 200;
-                high *= 2.0;
+        } else if (reqlev > 250) {
+            if (curlev <= 250) {
+                high = reqlev - 250;
+                high *= 1.5;
+                low = 250 - curlev;
+                low *= 1;
                 price = high + low;
-
-            }
-            if ((curlev <= 100) && (curlev > 0)) {
-                low = 100 - curlev;
-                low *= 1.0;
-                high = reqlev - 200;
-                high *= 2.0;
-                price = high + low + 150;
-
-            }
-
-        } else if (reqlev > 100 && reqlev <= 200) {
-
-            if ((curlev <= 200) && (curlev > 100)) {
+            } else if (curlev <= 350) {
                 price = reqlev - curlev;
                 price *= 1.5;
-
             }
-            if ((curlev <= 100) && (curlev > 0)) {
-                low = 100 - curlev;
-                low *= 1.0;
-                high = reqlev - 100;
-                high *= 1.5;
-                price = high + low;
-
-            }
-
-        } else if (reqlev > 1 && reqlev <= 850) {
-            if ((curlev <= 100) && (curlev > 0)) {
-                price = reqlev - curlev;
-                price *= 1.0;
-
-            }
+        } else if (reqlev > 1) {
+            price = reqlev - curlev;
+            price *= 1;
         }
 	return price;
 }
